caf2627a48cd1f043e412e318ec0b0c325971c1a
xuzongyao <m13221831059@163.com>
2018-06-28 16:04:54 +0800

    主页面

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import CMHeader from '../../components/Header/cmHeader.js';
-import {Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 // import rice form '../price/price';
 // import PS from '../price/price.js'
+import './login.css';
 class Login extends Component {
     render() {
         return (
-            <div>
+            <div className ="main">
                 <span><Link to="price">登陆</Link></span>
                 {/* <CMHeader /> */}
             </div>
